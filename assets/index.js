2051e2ce9fc9f3167ec06c2e1270afecf7f78ed5
Ghosted34 <greyman1934@outlook.com>
2023-08-17 04:16:09 +0100

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+export { default as memory } from "./icon-memory.svg";
+export { default as reaction } from "./icon-reaction.svg";
+export { default as verbal } from "./icon-verbal.svg";
+export { default as visual } from "./icon-visual.svg";
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import { Hanken_Grotesk } from "@next/font/google";
+import "@/styles/globals.css";
+
+const hanken = Hanken_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={hanken.className}>
+      <Component {...pageProps} />{" "}
+    </main>
+  );
 }
